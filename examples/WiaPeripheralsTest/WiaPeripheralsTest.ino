b3fad0cb1f8c24936c43d920b83f4de58debc05a
Szymon Masternak <51755583+szymonmasternak@users.noreply.github.com>
2019-08-06 12:00:48 +0100

    Changed to support DHT22
    Wia Peripherals now supports DHT22.

---
+++ examples/WiaPeripheralsTest/WiaPeripheralsTest.ino
@@ -0,0 +1,24 @@
+/* Example code for getting temperature, humidity from a Wia temperature shield */
+
+#include <WiaPeripherals.h>
+
+#define DHTPIN 22     
+#define DHTTYPE DHT22
+
+WiaPeripherals wiaPeripherals(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  wiaPeripherals.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+  Serial.print("Humidity: ");
+  Serial.println(wiaPeripherals.readHumidity());
+  Serial.print("Temperature Celsius: ");
+  Serial.println(wiaPeripherals.getTemperatureCelsius());
+  Serial.print("Temperature Fahrenheit: ");
+  Serial.println(wiaPeripherals.convertCtoF(wiaPeripherals.getTemperatureCelsius()));
+}
