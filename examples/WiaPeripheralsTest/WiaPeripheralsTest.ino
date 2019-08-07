86ebb9e70e96194872b8fa20c52dbce27b0feb0e
Szymon Masternak <51755583+szymonmasternak@users.noreply.github.com>
2019-08-07 11:46:48 +0100

    Changed Functions

---
+++ examples/WiaPeripheralsTest/WiaPeripheralsTest.ino
@@ -0,0 +1,24 @@
+/* Example code for getting temperature, humidity from a Wia temperature shield */
+
+#include <WiaPeripherals.h>
+
+#define DHTPIN 22     
+#define DHTTYPE DHT22
+
+WiaPeripherals wiaPeripherals;
+
+void setup() {
+  Serial.begin(9600);
+  wiaPeripherals.initTemperatureHumidity(DHTPIN, DHTTYPE);
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+  Serial.print("Humidity: ");
+  Serial.println(wiaPeripherals.getHumidity());
+  Serial.print("Temperature Celsius: ");
+  Serial.println(wiaPeripherals.getTemperatureCelsius());
+  Serial.print("Temperature Fahrenheit: ");
+  Serial.println(wiaPeripherals.convertCtoF(wiaPeripherals.getTemperatureCelsius()));
+}
